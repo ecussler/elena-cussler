8aab47adac616504d38df6b899e655181df2fc0a
E Cussler <cusslere@gmail.com>
2023-03-12 17:17:13 -0700

    Added email validation and ContactMe JSX

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import validator from 'validator'; 
+import { validateEmail } from '../utils/helpers';
 
 function ContactMe() {
     const [name, setName] = useState(''); 
@@ -7,29 +7,71 @@ function ContactMe() {
     const [emailError, setEmailError] = useState(''); 
     const [message, setMessage] = useState(''); 
 
-    const validateEmail = (e) => {
-        let email = e.target.value; 
-
-        return validator.isEmail(email) ? setEmailError('Valid email') : setEmailError('Please enter a valid email'); 
-    };
 
     const handleInputChange = (e) => {
-        const { name, value } = e.target; 
+        const { target } = e; 
+        const inputType = target.name; 
+        const inputValue = target.value; 
 
-        if (name === 'name') {
-            setName(value)
-        } else if (name === 'email') {
-            let email = e.target.value; 
-            return validator.isEmail(email) ? setEmailError('Valid email') : setEmailError('Please enter a valid email'); 
+        if (inputType === 'name') {
+            setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue); 
         }
-        // return name === 'name' ? setName(value) : setMessage(value); 
-
     }; 
 
     const handleFormSubmit = (e) => {
         e.preventDefault(); 
 
+        // Validate email input
+        if (!validateEmail(email)) {
+            setEmailError('Please enter a valid email address.'); 
+            return; 
+        }; 
 
-    }
+        setName(''); 
+        setEmail(''); 
+        setMessage(''); 
+    };
 
+    return (
+        <div>
+            <h3>Contact Me</h3>
+            <form className='contact-me'>
+                <input
+                  value={name}
+                  name='name'
+                  onChange={handleInputChange}
+                  type='text'
+                  placeholder='Name'
+                />
+                <input
+                  value={email}
+                  name='email'
+                  onChange={handleInputChange}
+                  type='email'
+                  placeholder='Email'
+                />
+                <input
+                  value={message}
+                  name='message'
+                  onChange={handleInputChange}
+                  type='text'
+                  placeholder='Please write a brief message here.'
+                />
+                <button type='button' onClick={handleFormSubmit}>
+                    Submit
+                </button>
+            </form>
+            {emailError && (
+                <div>
+                    <p className='error-message'>{emailError}</p>
+                </div>
+            )}
+        </div>
+    );
 }
+
+export default ContactMe
