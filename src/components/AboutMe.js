8b90d5e91472faefb8954c1c796ed81958ca29f1
E Cussler <cusslere@gmail.com>
2023-03-12 17:54:29 -0700

    Added Bootstrap component syling to ContactMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { Hero } from 'react-bootstrap'
+
+function AboutMe() {
+    return (
+        <div className='about-me'>
+            <Hero>
+                
+            </Hero>
+        </div>
+    )
+}
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap'; 
 import { validateEmail } from '../utils/helpers';
+
+
+const styles = {
+    button: {
+        
+    }
+}
+
+
 
 function ContactMe() {
     const [name, setName] = useState(''); 
@@ -39,32 +49,37 @@ function ContactMe() {
     return (
         <div>
             <h3>Contact Me</h3>
-            <form className='contact-me'>
-                <input
-                  value={name}
-                  name='name'
-                  onChange={handleInputChange}
-                  type='text'
-                  placeholder='Name'
-                />
-                <input
-                  value={email}
-                  name='email'
-                  onChange={handleInputChange}
-                  type='email'
-                  placeholder='Email'
-                />
-                <input
-                  value={message}
-                  name='message'
-                  onChange={handleInputChange}
-                  type='text'
-                  placeholder='Please write a brief message here.'
-                />
-                <button type='button' onClick={handleFormSubmit}>
-                    Submit
-                </button>
-            </form>
+            <Form className='contactMe'>
+                <Form.Group className='mb-3' controlId='contactMeForm'>
+                    <Form.Label class="form-label">Name</Form.Label>
+                    <input
+                    value={name}
+                    name='name'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Your Name'
+                    />
+                    <Form.Label class="form-label">Email address</Form.Label>
+                    <input
+                    value={email}
+                    name='email'
+                    onChange={handleInputChange}
+                    type='email'
+                    placeholder='example@email.com'
+                    />
+                    <Form.Label class="form-label">Message</Form.Label>
+                    <input
+                    value={message}
+                    name='message'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Please write a brief message here.'
+                    />
+                    <Button type='Button' onClick={handleFormSubmit}>
+                        Submit
+                    </Button>
+                </Form.Group>
+            </Form>
             {emailError && (
                 <div>
                     <p className='error-message'>{emailError}</p>
